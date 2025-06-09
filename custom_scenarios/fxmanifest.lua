117820d2b42c34b1a7963eba9ba138bd545967e8
Smokey <137696662+Robg815@users.noreply.github.com>
2025-06-09 02:03:51 -0500

    Add files via upload

---
+++ custom_scenarios/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+
+author 'Smokey'
+description 'Custom Scenarios Made for @Eddy'
+version '1.0'
+
+
+file "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+
+dependency '/assetpacks'
