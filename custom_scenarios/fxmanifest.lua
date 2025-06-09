61440a7c147ccc52fe9802e7181bafa3577149ac
Smokey <137696662+Robg815@users.noreply.github.com>
2025-06-09 15:14:03 -0500

    Update fxmanifest.lua

--- custom_scenarios/fxmanifest.lua
+++ custom_scenarios/fxmanifest.lua
@@ -3,8 +3,8 @@ game "gta5"
 
 author 'Smokey'
 description 'Custom Scenarios'
-version '1.0'
-
+version '1.0.0'
+server_script 'version.lua'
 
 file "sp_manifest.ymt"
 data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
